c613ca5871c2d8eac0159833a2722199d92100f4
Hung Duong Quang <qhungg289@protonmail.com>
2021-12-06 19:17:28 +0700

    Refactor

--- index.js
+++ index.js
@@ -4,46 +4,54 @@ const http = require("http");
 const port = process.env.port || 8080;
 
 const server = http.createServer((req, res) => {
-	if (req.url.toLowerCase() === "/") {
-		fs.readFile("./public/index.html", (err, data) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
+	switch (req.url.toLowerCase()) {
+		case "/":
+			fs.readFile("./public/index.html", (err, data) => {
+				if (err) {
+					console.error(err);
+					return;
+				}
 
-			res.writeHead(200, { "Content-Type": "text/html" });
-			res.end(data);
-		});
-	} else if (req.url.toLowerCase() === "/about") {
-		fs.readFile("./public/about.html", (err, data) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
+				res.writeHead(200, { "Content-Type": "text/html" });
+				res.end(data);
+			});
+			break;
 
-			res.writeHead(200, { "Content-Type": "text/html" });
-			res.end(data);
-		});
-	} else if (req.url.toLowerCase() === "/contact") {
-		fs.readFile("./public/contact-me.html", (err, data) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
+		case "/about":
+			fs.readFile("./public/about.html", (err, data) => {
+				if (err) {
+					console.error(err);
+					return;
+				}
 
-			res.writeHead(200, { "Content-Type": "text/html" });
-			res.end(data);
-		});
-	} else {
-		fs.readFile("./public/404.html", (err, data) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
+				res.writeHead(200, { "Content-Type": "text/html" });
+				res.end(data);
+			});
+			break;
 
-			res.writeHead(404, { "Content-Type": "text/html" });
-			res.end(data);
-		});
+		case "/contact":
+			fs.readFile("./public/contact-me.html", (err, data) => {
+				if (err) {
+					console.error(err);
+					return;
+				}
+
+				res.writeHead(200, { "Content-Type": "text/html" });
+				res.end(data);
+			});
+			break;
+
+		default:
+			fs.readFile("./public/404.html", (err, data) => {
+				if (err) {
+					console.error(err);
+					return;
+				}
+
+				res.writeHead(404, { "Content-Type": "text/html" });
+				res.end(data);
+			});
+			break;
 	}
 });
 
